9a43c0c7e3457a3ed5e3e7b7c5ea347c91c55ee4
elbiesmith <elbie.smith@gmail.com>
2021-07-22 17:57:24 -0500

    javascript

--- js/site.js
+++ js/site.js
@@ -1,62 +1,143 @@
 // create working task list
 let tasks = JSON.parse(localStorage.getItem('taskArray')) || [];
+// let buttonCell = `<button onclick="markCompleted(this)"><span class="text-primary"><i class="fas fa-check-square"></i></span></button>&nbsp; 
+//     <i class="fas fa-edit"></i>&nbsp;
+//     <span class="text-danger"><i class="fas fa-trash-alt"></span></i>
+//     `;
 
 // sample task data structure
 // let task = {
 //     title: 'string',
 //     taskDate: 'date String or date object',
-//     taskCompleted: bool,
-//     overdue: bool (maybe just check against current date when filtering)
+//     createdDate: 'date object',
+//     completed: bool
 // }
 
 function createTask() {
-    // build task object
+    // get user inputs
+    let taskField = document.getElementById('newTask');
+    let dateField = document.getElementById('newDateDue')
+    let task = taskField.value;
+    let createdDate = new Date().toLocaleDateString();
+    let date = new Date().toLocaleDateString();
 
-    // add to tasks 
+    let date2 = `${dateField.value} 00:00`
 
-    // update localStorage
+    // date is showing date - 1 day 
+    if (dateField.value == '') {
+        date = new Date().toLocaleDateString();
+    } else {
+        date = new Date(date2).toLocaleDateString();
+    }
+
+
+    if (task == '') {
+        Swal.fire('Please enter a task');
+    } else {
+        // build task object
+        let taskObject = {
+            title: task,
+            id: tasks.length,
+            taskDate: date,
+            created: createdDate,
+            completed: false,
+            buttonCell: `<button class='btn'id="${tasks.length}" onclick="markCompleted(this)" data-string="${tasks.length}"><span class="text-primary"><i class="fas fa-check-square"></i></span></button>&nbsp; 
+            <i class="fas fa-edit"></i>&nbsp;
+            <button class='btn' id="${tasks.length}" onclick="deleteTask(this)" data-string="${tasks.length}"><span class="text-danger"><i class="fas fa-trash-alt"></span></i></button>
+            `
+        }
+        // add to tasks 
+        tasks.push(taskObject);
+        // update localStorage
+        localStorage.setItem('taskArray', JSON.stringify(tasks))
+    }
+
+    closeModal();
+    display();
+
 }
 
 function clearTasks() {
     // set tasks to []
-
+    tasks = [];
     // update localStorage
+    localStorage.setItem('taskArray', JSON.stringify(tasks));
+    display();
 }
 
-function filter(task){
+function filter(task) {
     // sort tasks by filter
 
     // if theres a search term
     // check if term is in task
 
     // display screen using filtered items
-    
+
 }
 
-function display(filter){
+function display() {
+    const template = document.getElementById('taskTemplate');
+    const taskBody = document.getElementById('taskBody');
+    let workingData = tasks || [];
     // clear screen
-
+    taskBody.innerHTML = '';
     // defaults to all
+    for (let i = 0; i < workingData.length; i++) {
+        let taskRow = document.importNode(template.content, true);
+        taskRow.getElementById('task').innerHTML = workingData[i].title;
+        taskRow.getElementById('created').innerHTML = workingData[i].created;
+        taskRow.getElementById('dueDate').innerHTML = workingData[i].taskDate;
+        taskRow.getElementById('buttons').innerHTML = workingData[i].buttonCell;
+
+        taskBody.appendChild(taskRow);
+    }
+
 }
 
-function markCompleted(){
+function markCompleted(element) {
+    let elementId = parseInt(element.getAttribute('data-string'))
     // get this object from button
     // flip taskCompleted bool
     // change style to strikeout
+    
 }
 
 function editTask() {
+    let elementId = parseInt(element.getAttribute('data-string'))
     // get this object from button
     // open edit dialog
     // fill inputs with current data
 
     // if data is changed, change task
     // property
-    
+
     // update screen
 }
 
-function deleteTask() {
+function deleteTask(element) {
     // get index of this object from button
     // array.splice(index, 1) to remove
+    let elementId = parseInt(element.getAttribute('data-string'))
+    tasks.splice(elementId, 1);
+
+    // reset task indexes
+    for(let i = elementId; i < tasks.length; i++){
+        tasks[i].buttonCell = `<a id="${i}" onclick="markCompleted(this)" data-string="${i}"><span class="text-primary"><i class="fas fa-check-square"></i></span></a>&nbsp; 
+        <i class="fas fa-edit"></i>&nbsp;
+        <a id="${i}" onclick="deleteTask(this)" data-string="${i}"><span class="text-danger"><i class="fas fa-trash-alt"></span></i></button>
+        `
+    }
+
+    localStorage.setItem('taskArray', JSON.stringify(tasks));
+    display();
+
+
+}
+
+function closeModal() {
+    // let modal = new bootstrap.Modal(document.getElementById('exampleModal'), {});
+    // modal.hide();
+
+    let modalBtn = document.getElementById('closeModalBtn');
+    modalBtn.click();
 }
