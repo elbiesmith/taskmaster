d300177afe214b79cf175eb7e4438cd1d4edddf0
elbiesmith <elbie.smith@gmail.com>
2021-07-24 13:43:52 -0500

    searching and filtering

--- js/site.js
+++ js/site.js
@@ -1,6 +1,7 @@
 // create working task list
 let tasks = JSON.parse(localStorage.getItem('taskArray')) || [];
 let editIdNumber = 0;
+let workingData = tasks;
 // let buttonCell = `<button onclick="markCompleted(this)"><span class="text-primary"><i class="fas fa-check-square"></i></span></button>&nbsp; 
 //     <i class="fas fa-edit"></i>&nbsp;
 //     <span class="text-danger"><i class="fas fa-trash-alt"></span></i>
@@ -8,7 +9,7 @@ let editIdNumber = 0;
 
 // sample task data structure
 // let task = {
-//     title: 'string',
+//     task: 'string',
 //     taskDate: 'date String or date object',
 //     createdDate: 'date object',
 //     completed: bool
@@ -26,7 +27,7 @@ function createTask() {
 
     // date is showing date - 1 day 
     if (dateField.value == '') {
-        date = new Date(tasks[editIdNumber].taskDate).toLocaleDateString();
+        date = new Date().toLocaleDateString();
     } else {
         date = new Date(date2).toLocaleDateString();
     }
@@ -37,11 +38,12 @@ function createTask() {
     } else {
         // build task object
         let taskObject = {
-            title: task,
+            task: task,
             id: tasks.length,
             taskDate: date,
             created: createdDate,
             completed: false,
+            late: false,
             buttonCell: `<button class='btn' onclick="markCompleted(this)" data-string="${tasks.length}"><span class="text-primary"><i class="fas fa-check-square"></i></span></button>&nbsp; 
             <button class="btn" onclick="editTask(this)" data-string="${tasks.length}"><i class="fas fa-edit"></i></button>&nbsp;
             <button class='btn'  onclick="deleteTask(this)" data-string="${tasks.length}"><span class="text-danger"><i class="fas fa-trash-alt"></span></i></button>
@@ -55,7 +57,7 @@ function createTask() {
 
     closeModal();
     display();
-
+    clearForm('createForm');
 }
 
 function clearTasks() {
@@ -63,12 +65,49 @@ function clearTasks() {
     tasks = [];
     // update localStorage
     localStorage.setItem('taskArray', JSON.stringify(tasks));
+    workingData = tasks;
     display();
 }
 
 function filter(task) {
-    // sort tasks by filter
+    const template = document.getElementById('taskTemplate');
+    const taskBody = document.getElementById('taskBody');
 
+    taskBody.innerHTML = '';
+    
+    // sort tasks by filter
+    if(task == 'all'){
+        // display all
+        clearFilter();
+    } else if (task == 'incomplete') {
+        // loop and find completed == false
+        //display
+        workingData = [];
+
+        for( let i = 0; i < tasks.length; i++){
+            if(tasks[i].completed == false){
+                workingData.push(tasks[i]);
+            }
+        }
+
+        display();
+
+    } else if (task == 'completed') {
+        // loop and find completed == true
+        // display
+        workingData = [];
+
+        for( let i = 0; i < tasks.length; i++){
+            if(tasks[i].completed == true){
+                workingData.push(tasks[i]);
+            }
+        }
+
+        display();
+
+    } else if (task == 'late')  {
+
+    }
     // if theres a search term
     // check if term is in task
 
@@ -76,23 +115,27 @@ function filter(task) {
 
 }
 
+function clearFilter(){
+    workingData = tasks;
+    display();
+}
+
 function display() {
     const template = document.getElementById('taskTemplate');
     const taskBody = document.getElementById('taskBody');
-    let workingData = tasks || [];
     // clear screen
     taskBody.innerHTML = '';
     // defaults to all
     for (let i = 0; i < workingData.length; i++) {
         let taskRow = document.importNode(template.content, true);
         if (workingData[i].completed) {
-            taskRow.getElementById('task').innerHTML = `<span class="text-decoration-line-through">${workingData[i].title}</span>`;
+            taskRow.getElementById('task').innerHTML = `<span class="text-decoration-line-through">${workingData[i].task}</span>`;
             taskRow.getElementById('created').innerHTML = `<span class="text-decoration-line-through">${workingData[i].created}</span>`;
             taskRow.getElementById('dueDate').innerHTML = `<span class="text-decoration-line-through">${workingData[i].taskDate}</span>`;
             taskRow.getElementById('buttons').innerHTML = `<span>${workingData[i].buttonCell}</span>`;
         } else {
             
-            taskRow.getElementById('task').innerHTML = `<span class="text-decoration-none">${workingData[i].title}</span>`;
+            taskRow.getElementById('task').innerHTML = `<span class="text-decoration-none">${workingData[i].task}</span>`;
             taskRow.getElementById('created').innerHTML = `<span class="text-decoration-none">${workingData[i].created}</span>`;
             taskRow.getElementById('dueDate').innerHTML = `<span class="text-decoration-lnone">${workingData[i].taskDate}</span>`;
             taskRow.getElementById('buttons').innerHTML = `<span >${workingData[i].buttonCell}</span>`;
@@ -108,9 +151,21 @@ function markCompleted(element) {
     // flip taskCompleted bool
     if (tasks[elementId].completed == true) {
         tasks[elementId].completed = false;
+        for(let i = 0; i<workingData.length; i++){
+            if (workingData[i].id == elementId){
+                workingData[i].compelted = false;
+            }
+        }
     } else {
         tasks[elementId].completed = true;
+        for(let i = 0; i<workingData.length; i++){
+            if (workingData[i].id == elementId){
+                workingData[i].compelted = true;
+            }
+        }
     }
+
+    
     
     localStorage.setItem('taskArray', JSON.stringify(tasks));
     // change style to strikeout
@@ -125,7 +180,7 @@ function editTask(element) {
     editIdNumber = elementId;
     // fill inputs with current data
     let editTask = document.getElementById('editTask');
-    editTask.value = tasks[elementId].title;
+    editTask.value = tasks[elementId].task;
     let editDateDue = document.getElementById('editDateDue');
     editDateDue.value = formatDate(tasks[elementId].taskDate);
     
@@ -161,7 +216,7 @@ function updateTask(){
     } else {
         // build task object
         let taskObject = {
-            title: task,
+            task: task,
             id: tasks.length,
             taskDate: date,
             created: createdDate,
@@ -175,10 +230,18 @@ function updateTask(){
         tasks[editIdNumber] = taskObject;
         // update localStorage
         localStorage.setItem('taskArray', JSON.stringify(tasks))
+        for (let i = 0; i<workingData.length;i++){
+            if(workingData[i].id == taskObject.id){
+                workingData[i].task = task;
+            }
+        }
     }
 
+
+    
     closeEditModal();
     display();
+    clearForm('editForm');
     
 }
 
@@ -187,6 +250,13 @@ function deleteTask(element) {
     // array.splice(index, 1) to remove
     let elementId = parseInt(element.getAttribute('data-string'))
     tasks.splice(elementId, 1);
+    
+    for (let i = 0; i < workingData.length;i++){
+        if (workingData[i].id == elementId){
+            workingData.splice(i, 1);
+        }
+    }
+
 
     // reset task indexes
     for (let i = elementId; i < tasks.length; i++) {
@@ -233,6 +303,75 @@ function formatDate(dateString){
     return `${dateArray[2]}-${dateArray[0]}-${dateArray[1]} 00:00`
 }
 
+function clearForm(form){
+    document.getElementById(form).reset();
+}
+
+function setupEventListeners() {
+    document.getElementById("taskSave").addEventListener("click", createTask);
+    document.getElementById("clearFilter").addEventListener("click", clearFilter);
+    document.getElementById("editSave").addEventListener("click", function (e) {
+        e.preventDefault();
+        updateTask();
+    });
+    
+    document.getElementById("searchBtn").addEventListener("click", search)
+    document.getElementById("taskClear").addEventListener("click", clearTasks);
+    document.getElementById("menuClearTasks").addEventListener("click", clearTasks);
+    window.onload = display();
+    document.getElementById("newTask").addEventListener('keypress', function (keyPressed) {
+            
+        if (keyPressed.key === "Enter") {
+            keyPressed.preventDefault();
+            createTask();
+                
+        }
+    })
+    document.getElementById("newDateDue").addEventListener('keypress', function (keyPressed) {
+        if (keyPressed.key === "Enter") {
+            keyPressed.preventDefault();
+            createTask();
+        }
+    });
+    document.getElementById("editTask").addEventListener('keypress', function (keyPressed) {
+        if (keyPressed.key === "Enter") {
+            keyPressed.preventDefault();
+            updateTask();
+        }
+    });
+    document.getElementById("editDateDue").addEventListener('keypress', function (keyPressed) {
+        if (keyPressed.key === "Enter") {
+            keyPressed.preventDefault();
+            updateTask();
+        }
+    });
+    document.getElementById("displayAll").addEventListener("click", () => {
+        filter('all');
+    });
+    document.getElementById("filterIncomplete").addEventListener("click", () => {
+        filter('incomplete');
+    });
+    document.getElementById("filterCompleted").addEventListener("click", () => {
+        filter('completed');
+    });
+    document.getElementById("filterLate").addEventListener("click", () => {
+        filter('late');
+    });
+
+    
+}
+
+
+function search() {
+    let searchTerm = document.getElementById('searchBar').value;
+    workingData = [];
+    for(let i = 0; i < tasks.length; i++){
+        if(tasks[i].task.includes(searchTerm)){
+            workingData.push(tasks[i])
+        }
+    }
+    display();
+}
 // (x => x.something  == value)
 
 // is the same as 
